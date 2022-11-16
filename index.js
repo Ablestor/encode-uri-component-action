790ea01bcf2d5ca4d0dbe8c15351a87b47f22f61
Junseo Oh <insung9546@gmail.com>
2022-11-16 14:05:49 +0900

    Remove: remove lz-string

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const core = require("@actions/core");
-const LZString = require("lz-string");
 
 try {
   const string = core.getInput("string");
-  core.setOutput("string", LZString.compressToEncodedURIComponent(string));
+  core.setOutput("string", encodeURIComponent(string));
 } catch (error) {
   core.setFailed(error.message);
 }
