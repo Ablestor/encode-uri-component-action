95472df0cf7584dcaa042cad6b8e8ce8e8156d70
Junseo Oh <insung9546@gmail.com>
2022-11-16 13:21:59 +0900

    Feat: Add lz-string

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const core = require("@actions/core");
+const LZString = require("lz-string");
 
 try {
   const string = core.getInput("string");
-  core.setOutput("string", encodeURIComponent(string));
+  core.setOutput("string", LZString.compressToEncodedURIComponent(string));
 } catch (error) {
   core.setFailed(error.message);
 }
